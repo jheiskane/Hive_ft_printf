015ef11557ce11770a48abeabf5a247ec4f809e9
jheiskane <Juha@Juhas-Macbook.local>
2022-01-27 14:35:57 +0200

    Added print func for char

---
+++ print_c.c
@@ -0,0 +1,14 @@
+#include "ft_printf.h"
+
+void print_char(t_printf *tab)
+{
+	char a;
+
+	a = va_arg(tab->args, int);
+	//special_cases(tab, 1);
+	if (tab->width && !tab->dash)
+		right_align(tab, 1);
+	tab->b_written += write(1, &a, 1);
+	if (tab->width && tab->dash)
+		left_align(tab, 1);
+}

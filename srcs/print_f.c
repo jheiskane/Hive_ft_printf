2be4cc7e59508a6a5199f467147e85e06ae534b8
Juha <viderus@gmail.com>
2022-05-04 15:26:52 +0300

    Added hash flash functionality

--- srcs/print_f.c
+++ srcs/print_f.c
@@ -6,7 +6,7 @@
 /*   By: jheiskan <jheiskan@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/14 17:13:26 by jheiskan          #+#    #+#             */
-/*   Updated: 2022/05/04 14:07:16 by jheiskan         ###   ########.fr       */
+/*   Updated: 2022/05/04 15:26:32 by jheiskan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,10 @@ void	print_f(t_printf *tab)
 {
 	char	*s;
 	char	*tmp;
+	int		dot_printed;
 
 	s = ft_itoa_float(tab, special_cases_f(tab));
+	dot_printed = 0;
 	if (!s)
 		exit(-1);
 	tmp = s;
@@ -25,7 +27,13 @@ void	print_f(t_printf *tab)
 	if (tab->width > 0 && !tab->dash)
 		s = align_f(tab, tab->width, ' ', s);
 	while (*s)
+	{
+		if (*s == '.')
+			dot_printed = 1;
 		tab->b_written += write(1, &*s++, 1);
+	}
+	if (!dot_printed && tab->hash)
+		tab->b_written += write(1, ".", 1);
 	if (tab->width > 0 && tab->dash)
 		s = align_f(tab, tab->width, ' ', s);
 	free (tmp);
